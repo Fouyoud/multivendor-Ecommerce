3e277f5b00b716eb3bb9d0db6901cca5ce271d6d
Fouyoud <kenbibi39@gmail.com>
2025-01-26 03:41:35 +0100

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-   <div>
-    <h1>Welcome into Next.js</h1>
+   <div className="p-5">
+      <div className="w-100 flex justify-end">
+          <ThemeToggle/>
+      </div>
+        <div>
+          <h1 className="fon-bold text-blue-500 font-barlow">Welcome into Next.js</h1>
+       </div>
+         <Button>Click here</Button>
    </div>
   );
 }
